adbedcd9bf4fe81cd09283d67fca9052d416c11a
fyozrs <mkostovska33@gmail.com>
2026-09-12 18:52:43 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './css/App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
